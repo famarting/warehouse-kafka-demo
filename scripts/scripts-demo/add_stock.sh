03959a2c6f73fe07421ed750370c2e48ac9b7bae
Fabian Martinez <famartin@redhat.com>
2020-09-09 23:10:49 +0200

    fix scripts

--- scripts/scripts-demo/add_stock.sh
+++ scripts/scripts-demo/add_stock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 QUANTITY=${1:-5}
 
-curl -i -d '{"item-id":"123456", "quantity":'"$QUANTITY"'}' -H "Content-Type: application/json" -X POST http://$(oc get route warehouse-service --template='{{ .spec.host }}')/warehouse/stocks
+curl -i -d '{"itemId":"123456", "quantity":'"$QUANTITY"'}' -H "Content-Type: application/json" -X POST http://$(oc get route warehouse-service --template='{{ .spec.host }}')/warehouse/stocks
 echo
--- scripts/scripts-demo/send_orders.sh
+++ scripts/scripts-demo/send_orders.sh
@@ -4,6 +4,6 @@ QUANTITY_PER_ORDER=${2:-1}
 
 for i in $(seq 1 $NUM_OF_ORDERS)
 do
-    curl -i -d '{"item-id":"123456", "quantity":'"$QUANTITY_PER_ORDER"'}' -H "Content-Type: application/json" -X POST http://$(oc get route warehouse-service --template='{{ .spec.host }}')/orders
+    curl -i -d '{"itemId":"123456", "quantity":'"$QUANTITY_PER_ORDER"'}' -H "Content-Type: application/json" -X POST http://$(oc get route warehouse-service --template='{{ .spec.host }}')/orders
     echo
 done
